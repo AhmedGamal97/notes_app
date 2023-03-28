18c2ded14100173320914a1b33447c55db7518fc
AhmedGamal97 <ag469389@gmail.com>
2023-03-28 20:42:06 +0200

    add edit app bar

--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: const [
+            CustomAppBar(title: 'Edit Note', icon: Icons.check),
+          ],
+        ),
+      ),
+    );
   }
 }
